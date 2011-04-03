d289d9a99200694b918f9758fb3c461803ed26d6
David Baumgold <baumgold@brandeis.edu>
2011-04-03 12:48:15 -0400

    test_call view requires a logged-in user, logs who requested the call

--- buscall/views/twilio.py
+++ buscall/views/twilio.py
@@ -1,6 +1,8 @@
 from buscall import app
 import twilio_api as tw
-from buscall.credentials import \
+from google.appengine.api import users
+from ..decorators import login_required
+from ..credentials import \
 	ACCOUNT_SID, ACCOUNT_TOKEN, CALLER_ID
 
 API_VERSION = "2010-04-01"
@@ -9,7 +11,10 @@ account = tw.Account(ACCOUNT_SID, ACCOUNT_TOKEN)
 
 @app.route("/call")
 @app.route("/call/<int:phone_num>")
+@login_required
 def test_call(phone_num="3015238533"):
+	user = users.get_current_user()
+	app.logger.info("%s (%s) called %s" % (user.nickname(), user.user_id(), phone_num))
 	call_info = {
 		'From': CALLER_ID,
 		'To': phone_num,
