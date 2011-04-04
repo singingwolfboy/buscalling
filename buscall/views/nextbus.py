851288971ea38707c270f14fff4067bfea517c96
David Baumgold <baumgold@brandeis.edu>
2011-04-04 18:20:39 -0400

    Put in infrastructure for generating TwiML

--- buscall/views/nextbus.py
+++ buscall/views/nextbus.py
@@ -1,6 +1,7 @@
 from buscall import app
 from flask import render_template
 from buscall.models import nextbus
+import twilio_api as tw
 
 @app.route('/routes')
 def index_routes():
@@ -14,9 +15,21 @@ def show_route(route_id):
     return render_template('routes/show.html', route=route, directions=directions)
 
 @app.route('/predict/<route_id>/<stop_id>')
+@app.route('/predict/<route_id>/<stop_id>.<format>')
 @app.route('/predict/<route_id>/<stop_id>/<dir_id>')
-def predict_for_stop(route_id, stop_id, dir_id=None):
+@app.route('/predict/<route_id>/<stop_id>/<dir_id>.<format>')
+def predict_for_stop(route_id, stop_id, dir_id=None, format="html"):
     route = nextbus.get_route(route_id)
     stop = route['stops'][stop_id]
     predictions = nextbus.get_predictions(route_id, stop_id, dir_id)
-    return render_template('routes/predict.html', route=route, stop=stop, predictions=predictions)
+    if format.lower() == "twiml":
+        # only support one direction for phone calls.
+        prediction = predictions[dir_id]
+        return get_twiml(route, stop, prediction)
+    else:
+        return render_template('routes/predict.html', route=route, stop=stop, predictions=predictions)
+
+def get_twiml(route, stop, prediction):
+    r = tw.Response()
+    r.addSay("%s minutes until %s bus arrives at %s, heading towards %s.")
+    return r
