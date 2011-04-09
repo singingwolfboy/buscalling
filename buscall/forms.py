303c54b7db02886104f75a21e26c2fe2afe65b14
David Baumgold <baumgold@brandeis.edu>
2011-04-09 08:48:54 -0400

    Changed WaitlistForm from model_form to standard form

--- buscall/forms.py
+++ buscall/forms.py
@@ -1,5 +1,27 @@
-#from flaskext import wtf
-from buscall.models import WaitlistEntry
-from wtforms.ext.appengine.db import model_form
+#from buscall.models import WaitlistEntry
+#from .widgets import GeoPtHiddenInput, ModelConverter
+#from wtforms.ext.appengine.db import model_form
+from wtforms.form import Form
+from wtforms import fields as f
+from wtforms import validators as v
+from wtforms import widgets as w
 
-WaitlistForm = model_form(WaitlistEntry)
+# split location hidden fields is too hard for WTForms, we'll handle separately
+#WaitlistForm = model_form(WaitlistEntry, exclude=('location'), field_args={
+#    'email': {
+#        'default': 'email@example.com',
+#    }
+#})
+
+class WaitlistForm(Form):
+    email = f.TextField(u'Email', default='email@example.com',
+        validators=[v.required(), v.email()])
+    location_lat  = f.DecimalField(u'Latitude', widget=w.HiddenInput(),
+        validators=[v.optional()])
+    location_long = f.DecimalField(u'Longitude', widget=w.HiddenInput(),
+        validators=[v.optional()])
+
+    def validate_email(form, field):
+        if field.data == field.default:
+            field.message = u'%s cannot be %s' % (field.label.text, field.data)
+            raise v.ValidationError(field.message)
