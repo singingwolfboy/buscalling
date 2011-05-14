138eff5efa1f43137d6db74224ac00fdd0fb8b99
David Baumgold <baumgold@brandeis.edu>
2011-05-14 15:36:10 -0400

    restructured libs to use Flask-WTF, rewrote forms.py to take advantage of it

--- buscall/forms.py
+++ buscall/forms.py
@@ -1,37 +1,15 @@
-#from buscall.models import WaitlistEntry
-#from .widgets import GeoPtHiddenInput, ModelConverter
-#from wtforms.ext.appengine.db import model_form
-from wtforms.form import Form
-from wtforms import fields as f
-from wtforms import validators as v
-from wtforms import widgets as w
+from flaskext.wtf import Form, DecimalField, HiddenInput
+from flaskext.wtf import Required, Optional
+from flaskext.wtf.html5 import EmailField
 
-# split location hidden fields is too hard for WTForms, we'll handle separately
-#WaitlistForm = model_form(WaitlistEntry, exclude=('location'), field_args={
-#    'email': {
-#        'default': 'email@example.com',
-#    }
-#})
-
-class EmailInput(w.Input):
-    input_type = "email"
-
-class EmailField(f.TextField):
-    widget = EmailInput()
-
-    def __init__(self, *args, **kwargs):
-        validators = kwargs.get('validators', [])
-        validators.append(v.email())
-        kwargs['validators'] = validators
-        super(EmailField, self).__init__(*args, **kwargs)
 
 class WaitlistForm(Form):
     email = EmailField(u'Email', default='email@example.com',
-        validators=[v.required()])
-    location_lat  = f.DecimalField(u'Latitude', widget=w.HiddenInput(),
-        validators=[v.optional()])
-    location_long = f.DecimalField(u'Longitude', widget=w.HiddenInput(),
-        validators=[v.optional()])
+        validators=[Required()])
+    location_lat  = DecimalField(u'Latitude', widget=HiddenInput(),
+        validators=[Optional()])
+    location_long = DecimalField(u'Longitude', widget=HiddenInput(),
+        validators=[Optional()])
 
     def validate_email(form, field):
         if field.data == field.default:
