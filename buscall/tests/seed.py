e17d9242499a98e2f3fb58ed08462b1c874285f2
David Baumgold <baumgold@brandeis.edu>
2011-07-02 16:07:58 -0400

    Added root and lib dirs to python path, no longer need to add them dynamically

--- buscall/tests/seed.py
+++ buscall/tests/seed.py
@@ -1,14 +1,7 @@
 #!/opt/local/bin/python2.5
-from __future__ import absolute_import
-import os
-import sys
-dirname = os.path.dirname
-root_path = dirname(dirname(dirname(os.path.abspath(__file__))))
-lib_path = os.path.join(root_path, 'lib')
-sys.path.insert(0, lib_path)
-sys.path.insert(0, root_path)
 
 # flush old data
+import os
 from buscall.util import DATASTORE_FILE
 if os.path.exists(DATASTORE_FILE):
     os.remove(DATASTORE_FILE)
