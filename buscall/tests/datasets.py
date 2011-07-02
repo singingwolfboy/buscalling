a85028ca2b09d2ddf045049900eee4f08956efa9
David Baumgold <baumgold@brandeis.edu>
2011-07-02 16:07:12 -0400

    Fixed datasets to use seen rather than end

--- buscall/tests/datasets.py
+++ buscall/tests/datasets.py
@@ -8,7 +8,7 @@ test_user = users.User("test@example.com")
 class BusListenerData(DataSet):
     class morning_bus:
         user = test_user
-        agency_id = "MBTA"
+        agency_id = "mbta"
         route_id = "556"
         direction_id = "556_5560006v0_1"
         stop_id = "77378"
@@ -20,11 +20,11 @@ class BusListenerData(DataSet):
         sat = False
         sun = False
         start = datetime.time(8, 0) # 8:00 AM
-        end   = datetime.time(8,30) # 8:30 AM
+        seen = False
 
     class afternoon_bus:
         user = test_user
-        agency_id = "MBTA"
+        agency_id = "mbta"
         route_id = "59"
         direction_id = "59_590008v0_1"
         stop_id = "82048"
@@ -36,4 +36,27 @@ class BusListenerData(DataSet):
         sat = False
         sun = False
         start = datetime.time(16,45) # 4:45 PM
-        end   = datetime.time(17,10) # 5:10 PM
+        seen = False
+
+    class cron_bus:
+        user = test_user
+        agency_id = "mbta"
+        route_id = "26"
+        direction_id = "26_1_var1"
+        stop_id = "492"
+        mon = False
+        tue = False
+        wed = False
+        thu = False
+        fri = False
+        sat = True
+        sun = True
+        start = datetime.time(15,00) # 3:00 PM
+        seen = False
+
+class BusAlertData(DataSet):
+    class cron_bus_20_min:
+        listener = cron_bus
+        minutes = 20
+        medium = "email"
+        executed = False
