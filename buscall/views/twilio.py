d7a102cee48bc2d54541075b2e4eefce73c0cb22
David Baumgold <baumgold@brandeis.edu>
2011-07-15 07:32:39 -0400

    spaces to tabs, added sms_prediction method

--- buscall/views/twilio.py
+++ buscall/views/twilio.py
@@ -4,9 +4,10 @@ from buscall import app
 from twilio_api import Account, HTTPErrorAppEngine
 import simplejson as json
 from google.appengine.api import users
-from buscall.decorators import login_required
+from buscall.models.nextbus import get_predictions, get_route, get_stop
+from buscall.decorators import admin_required
 from buscall.credentials import ACCOUNT_SID, ACCOUNT_TOKEN, PHONE_NUMBER
-from buscall.util import DOMAIN
+from buscall.util import DOMAIN, pluralize_minutes
 from flask import url_for
 
 API_VERSION = "2010-04-01"
@@ -14,32 +15,74 @@ account = Account(ACCOUNT_SID, ACCOUNT_TOKEN)
 
 @app.route("/call/<agency_id>/<route_id>/<direction_id>/<stop_id>/<phone_num>")
 @app.route("/<agency_id>/routes/<route_id>/directions/<direction_id>/stops/<stop_id>/call/<phone_num>")
-@login_required
+@admin_required
 def call_prediction(agency_id, route_id, direction_id, stop_id, phone_num):
-	user = users.get_current_user()
-	app.logger.info("%s (%s) called %s" % (user.nickname(), user.user_id(), phone_num))
-	url = DOMAIN + url_for('predict_for_stop', agency_id=agency_id, 
-		route_id=route_id, direction_id=direction_id, stop_id=stop_id, format="twiml")
-	call_info = {
-		'From': PHONE_NUMBER,
-		'To': phone_num,
-		'Url': url,
-		'Method': 'GET',
-	}
-	try:
-		call_json = account.request(
-			'/%s/Accounts/%s/Calls.json' % (API_VERSION, ACCOUNT_SID),
-			'POST', 
-			call_info)
-		app.logger.info(call_json)
-		call = json.loads(call_json)
-		return "Now calling %s with call ID %s" % (call['to'], call['sid'])
-	except HTTPErrorAppEngine, e:
-		app.logger.error(e)
-		try:
-			err = json.loads(e.msg)
-			message = err['Message']
-			return "REMOTE ERROR: %s" % (message,)
-		except:
-			return "Couldn't parse error output:<br>\n%s" % e.msg
+    user = users.get_current_user()
+    app.logger.info("%s (%s) called %s" % (user.nickname(), user.user_id(), phone_num))
+    url = DOMAIN + url_for('predict_for_stop', agency_id=agency_id, 
+        route_id=route_id, direction_id=direction_id, stop_id=stop_id, format="twiml")
+    call_info = {
+        'From': PHONE_NUMBER,
+        'To': phone_num,
+        'Url': url,
+        'Method': 'GET',
+    }
+    try:
+        call_json = account.request(
+            '/%s/Accounts/%s/Calls.json' % (API_VERSION, ACCOUNT_SID),
+            'POST', 
+            call_info)
+        app.logger.info(call_json)
+        call = json.loads(call_json)
+        return "Now calling %s with call ID %s" % (call['to'], call['sid'])
+    except HTTPErrorAppEngine, e:
+        app.logger.error(e)
+        try:
+            err = json.loads(e.msg)
+            message = err['Message']
+            return "REMOTE ERROR: %s" % (message,)
+        except:
+            return "Couldn't parse error output:<br>\n%s" % e.msg
 
+@app.route("/sms/<agency_id>/<route_id>/<direction_id>/<stop_id>/<phone_num>")
+@app.route("/<agency_id>/routes/<route_id>/directions/<direction_id>/stops/<stop_id>/sms/<phone_num>")
+@admin_required
+def sms_prediction(agency_id, route_id, direction_id, stop_id, phone_num):
+    user = users.get_current_user()
+    app.logger.info("%s (%s) texted %s" % (user.nickname(), user.user_id(), phone_num))
+    prediction = get_predictions(agency_id, route_id, direction_id, stop_id)
+    route = get_route(agency_id, route_id)
+    stop = get_stop(agency_id, route_id, direction_id, stop_id)
+    if len(prediction.buses) > 1:
+        first = prediction.buses[0]
+        rest = prediction.buses[1:]
+        body = "%s until %s arrives at %s. %d more buses: %s." % (
+            pluralize_minutes(first.minutes), 
+            route.title, 
+            stop.title,
+            len(rest),
+            ", ".join([pluralize_minutes(bus.minutes) for bus in rest]),
+        )
+    elif prediction.buses == 1:
+        first = prediction.buses[0]
+        next = prediction.buses[1]
+        body = "%s until %s arrives at %s. One more bus in %s." % (
+            pluralize_minutes(first.minutes),
+            route.title,
+            stop.title,
+            pluralize_minutes(next.minutes),
+        )
+    else:
+        body = "No buses predicted (%s, %s)" % (listener.route.title, listener.stop.title)
+
+    sms_info = {
+        'From': PHONE_NUMBER,
+        'To': phone_num,
+        'Body': body,
+    }
+    sms = json.loads(account.request(
+        '/%s/Accounts/%s/SMS/Messages.json' % (API_VERSION, ACCOUNT_SID),
+        'POST',
+        sms_info,
+    ))
+    return "Now texting %s with sms ID %s" % (sms['to'], sms['sid'])
