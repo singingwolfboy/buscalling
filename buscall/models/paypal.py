2a7963d709529bc7fb480bd42b60e11522d1dbb5
David Baumgold <baumgold@brandeis.edu>
2011-07-27 21:36:17 -0400

    Added sandbox boolean for paypal model

--- buscall/models/paypal.py
+++ buscall/models/paypal.py
@@ -3,12 +3,14 @@ from buscall import app
 #if app.debug:
 if True:
     # sandbox info
+    sandbox = True
     url = "https://www.sandbox.paypal.com/cgi-bin/webscr"
     pdt_token = "VXASrbamQDoW0olGqyfA7Ty-HdawXzNfCfvbGQ_83Dv1Ecle4PF7Wpe8oP0"
     subscribe_button_id = "GUUGPYAWQ63CG"
     unsubscribe_button_id = "BPQAK7D34287Q"
 else:
     # real info
+    sandbox = False
     url = "https://www.paypal.com/cgi-bin/webscr"
     pdt_token = "SWCM2tf41PckuxT-MNYirYukaWXdkTS9kWIqmSNMDKrGzXHstbhO8RGZ5pu"
     subscribe_button_id = "W42Q7VA9H8USQ"
