db8b1e0b75c818c106955020ce72b5dc35969c3a
David Baumgold <singingwolfboy@gmail.com>
2011-11-08 21:59:07 -0500

    Updated gae_pdb

--- lib/gae_pdb.py
+++ lib/gae_pdb.py
@@ -1,6 +1,12 @@
+import pdb, sys
+
 def set_trace():
-    import pdb, sys
-    sys.stdout = sys.__stdout__
-    sys.stdin = sys.__stdin__
-    debugger = pdb.Pdb()
-    debugger.set_trace(sys._getframe().f_back)
+    sys.stdout, sys.__stdout__ = sys.__stdout__, sys.stdout
+    sys.stdin, sys.__stdin__ = sys.__stdin__, sys.stdin
+    sys.stderr, sys.__stderr__ = sys.__stderr__, sys.stderr
+    # debugger = pdb.Pdb()
+    # debugger.set_trace(sys._getframe().f_back)
+    pdb.set_trace()
+    sys.stdout, sys.__stdout__ = sys.__stdout__, sys.stdout
+    sys.stdin, sys.__stdin__ = sys.__stdin__, sys.stdin
+    sys.stderr, sys.__stderr__ = sys.__stderr__, sys.stderr
