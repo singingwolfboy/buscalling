192439ddcf37eba425c306876ebeb7ffd9a032e3
David Baumgold <singingwolfboy@gmail.com>
2011-12-03 18:11:40 -0500

    Added logging to cache_nextbus script

--- buscall/tests/urlfetch/cache_nextbus.py
+++ buscall/tests/urlfetch/cache_nextbus.py
@@ -4,22 +4,30 @@ from requests import async
 from lxml import etree
 from urllib import urlencode
 import os
+import logging
 
 NEXTBUS_URL = "http://webservices.nextbus.com/service/publicXMLFeed?"
 FILE_ROOT = os.path.join(os.path.dirname(__file__), "nextbus_api")
+
+logger = logging.getLogger('cache_nextbus')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+formatter = logging.Formatter()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 def raw_headers(response):
     headers = response._resp.info().headers
     return "".join([h.replace("\r", "") for h in headers])
 
-def log_response(response, file):
+def save_response(response, file):
     with open(file, "w") as f:
-        f.write(raw_headers(response.content))
+        f.write(raw_headers(response))
         f.write("\n")
         f.write(response.content.replace("\r", ""))
 
 def handle_agencylist(response):
-    log_response(response, os.path.join(FILE_ROOT, "agency_list.xml"))
+    save_response(response, os.path.join(FILE_ROOT, "agency_list.xml"))
     agencies_tree = etree.fromstring(response.content)
     agency_ids = agencies_tree.xpath('//agency/@tag')
     requests = []
@@ -31,6 +39,7 @@ def handle_agencylist(response):
         hooks = {
             "response": get_routelist_handler(agency_id),
         }
+        logger.info(url)
         requests.append(async.get(url, hooks=hooks))
     async.map(requests)
 
@@ -39,7 +48,7 @@ def get_routelist_handler(agency_id):
         dir = os.path.join(FILE_ROOT, agency_id)
         if not os.path.exists(dir):
             os.mkdir(dir)
-        log_response(response, os.path.join(dir, "route_list.xml"))
+        save_response(response, os.path.join(dir, "route_list.xml"))
         routelist_tree = etree.fromstring(response.content)
         route_ids = routelist_tree.xpath('//route/@tag')
         requests = []
@@ -52,6 +61,7 @@ def get_routelist_handler(agency_id):
             hooks = {
                 "response": get_routeconfig_handler(agency_id, route_id),
             }
+            logger.info(url)
             requests.append(async.get(url, hooks=hooks))
         async.map(requests)
     return handle_routelist
@@ -61,10 +71,11 @@ def get_routeconfig_handler(agency_id, route_id):
         dir = os.path.join(FILE_ROOT, agency_id, route_id)
         if not os.path.exists(dir):
             os.mkdir(dir)
-        log_response(response, os.path.join(dir, "config.xml"))
+        save_response(response, os.path.join(dir, "config.xml"))
     return handle_routeconfig
 
 if __name__ == "__main__":
     url = NEXTBUS_URL + urlencode({"command": "agencyList"})
     hooks = dict(response=handle_agencylist)
+    logger.info(url)
     requests.get(url, hooks=hooks)
