05619a4331f2c330705973ccf4d3e1c6a356dc40
David Baumgold <david@davidbaumgold.com>
2012-01-19 23:14:00 -0500

    Add smoke tests for stops

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -54,13 +54,17 @@ class SmokeTestCase(CustomTestCase):
         agency = json.loads(agency_resp.data)
         self.assertIsInstance(agency['routes'], dict)
 
-        rv = self.app.get('/agencies/mbta/routes?format=json')
+        rv = get_json('/agencies/mbta/routes')
         assert rv.status.startswith("200"), rv.status
+        self.assertEqual(rv.data,
+                self.app.get('/agencies/mbta/routes?format=json').data)
         routes = json.loads(rv.data)
         self.assertIsInstance(routes, list)
 
         r70_resp = get_json('/agencies/mbta/routes/70')
         assert r70_resp.status.startswith("200"), r70_resp.status
+        self.assertEqual(r70_resp.data,
+                self.app.get('/agencies/mbta/routes/70?format=json').data)
         r70 = json.loads(r70_resp.data)
         self.assertIsInstance(r70, dict)
         self.assertEqual(r70['id'], "70")
@@ -69,9 +73,9 @@ class SmokeTestCase(CustomTestCase):
         get_json = partial(self.app.get, headers={"Accept": "application/json"})
 
         # before it's loaded into the datastore, should get a 404
-        rv = self.app.get('/agencies/mbta/routes/70/directions?format=json')
+        rv = get_json('/agencies/mbta/routes/70/directions')
         assert rv.status.startswith("404"), rv.status
-        rv = self.app.get('/agencies/mbta/routes/70/directions/70_0_var0?format=json')
+        rv = get_json('/agencies/mbta/routes/70/directions/70_0_var0')
         assert rv.status.startswith("404"), rv.status
 
         # load it in
@@ -98,3 +102,29 @@ class SmokeTestCase(CustomTestCase):
         self.assertIsInstance(direction, dict)
         self.assertEqual(direction['id'], "70_0_var0")
 
+    def test_api_stops(self):
+        get_json = partial(self.app.get, headers={"Accept": "application/json"})
+
+        # before it's loaded into the datastore, should get a 404
+        rv = get_json('/agencies/mbta/routes/70/directions/70_0_var0/stops')
+        assert rv.status.startswith("404"), rv.status
+        rv = get_json('/agencies/mbta/routes/70/directions/70_0_var0/stops/8820')
+        assert rv.status.startswith("404"), rv.status
+
+        # load it in
+        self.build_entities_from_urlfetch_files(agencies="mbta", routes="70", directions="70_0_var0", stops=True)
+
+        rv = get_json('agencies/mbta/routes/70/directions/70_0_var0/stops')
+        assert rv.status.startswith("200"), rv.status
+        self.assertEqual(rv.data,
+                self.app.get('agencies/mbta/routes/70/directions/70_0_var0/stops?format=json').data)
+        stops = json.loads(rv.data)
+        self.assertIsInstance(stops, list)
+
+        stop_resp = get_json('/agencies/mbta/routes/70/directions/70_0_var0/stops/8820')
+        assert stop_resp.status.startswith("200"), stop_resp.status
+        self.assertEqual(stop_resp.data,
+                self.app.get('/agencies/mbta/routes/70/directions/70_0_var0/stops/8820?format=json').data)
+        stop = json.loads(stop_resp.data)
+        self.assertIsInstance(stop, dict)
+        self.assertEqual(stop['id'], "8820")
